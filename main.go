fc68b74dd2f2633c7387d04be190757489548e86
lmatus <levi.x.matus@nielsen.com>
2019-10-08 11:35:24 -0300

    change main function to produce new sounds to be played

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/lmatus/soundboard/pkg/webhook"
+	"github.com/LeviMatus/soundboard/pkg/sounds"
+	"github.com/LeviMatus/soundboard/pkg/webhook"
 	"log"
 	"net/http"
 )
 
+var samples = make(chan string, 10)
+
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	log.Println("got here")
+	samples <- "ending.mp3"
 	var b webhook.Webhook
 	err := json.NewDecoder(r.Body).Decode(&b)
 
@@ -22,6 +27,7 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	go sounds.Consume(samples)
 	log.Println("server started")
 	http.HandleFunc("/webhook", handleWebhook)
 	log.Fatal(http.ListenAndServe(":8080", nil))
